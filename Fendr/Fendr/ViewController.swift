f80e64d61f798fa9cf27018fae166ec5b7f8f634
Chang Liu <bobbyliu117@gmail.com>
2017-10-24 19:01:26 -0400

    Project Created

---
+++ Fendr/Fendr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fendr
+//
+//  Created by Chang Liu on 10/24/17.
+//  Copyright © 2017 FSMobileDVP4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
